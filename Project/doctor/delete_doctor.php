1a794c332b55189119d55ff60c715046abb9cb5d
JordanCBuckley <115782258+JordanCBuckley@users.noreply.github.com>
2023-05-14 18:54:41 +0100

    Add files via upload

---
+++ Project/doctor/delete_doctor.php
@@ -0,0 +1,20 @@
+<?php include('navbar_doctor.php');?>
+
+<?php
+if (isset($_GET["userID"])){
+    $userID = $_GET["userID"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "project database";
+
+    //create connection
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM doctorsinformation WHERE userID=$userID";
+    $connection->query($sql);
+}
+header("location: /project/index_doctor.php");
+exit;
+?>
